4370decea706ee78e02295324353ca0c1f3dd753
soohyn <sooheyonjo@gmail.com>
2025-04-17 23:25:21 +0900

    [Feat] 로그인 기능 개발
    - sessionStrage get, set, remove 관련 함수 작성
    
    Resolves 로그인 기능 구현 #2

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,13 +1,31 @@
-import { supabaseClient } from "./supabaseClient"
+import { Session } from "@supabase/supabase-js";
+import { supabaseClient } from "./supabaseClient";
 
-export async function getSession () {
-  try{
-    const response = await supabaseClient().auth.getSession()
-    
-    if(response.error) throw new Error();
+const STORAGE_KEY = 'session'
 
-    return response.data.session
-  }catch(error){
+export async function getSession() {
+  try {
+    const storageSession = sessionStorage.getItem(STORAGE_KEY)
+
+    if(storageSession){
+      const session = JSON.parse(storageSession)
+      return session
+    }
+
+    const response = await supabaseClient().auth.getSession();
+
+    if (response.error) throw new Error();
+
+    return response.data.session;
+  } catch (error) {
     console.error(error);
   }
+}
+
+export const setSessionStorage = (session:Session)=>{
+  sessionStorage.setItem(STORAGE_KEY,JSON.stringify(session))
+}
+
+export const removeSessionStorage =()=>{
+  sessionStorage.removeItem(STORAGE_KEY)
 }
--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,7 +1,49 @@
+import { useNavigate } from "react-router";
+import Input from "../components/Input";
+import { supabaseClient } from "../lib/supabaseClient";
+import { setSessionStorage } from "../lib/user";
+
 function SignIn() {
-  return <section>
-    <h2>로그인 페이지</h2>
-  </section>
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData(e.currentTarget);
+      const email = formData.get("email");
+      const password = formData.get("password");
+
+      if (!email || !password) {
+        alert("아이디와 비밀번호를 모두 작성해 주세요");
+        return;
+      }
+
+      const response = await supabaseClient().auth.signInWithPassword({
+        email: email.toString(),
+        password: password.toString(),
+      });
+
+      if (response.error) {
+        alert("아이디 또는 비밀번호를 확인해 주세요");
+        throw new Error();
+      }
+
+      setSessionStorage(response.data.session);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <section>
+      <h2>로그인 페이지</h2>
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        <Input label={"이메일"} autoComplete="email" type="email" name="email" placeholder="이메일" />
+        <Input label={"비밀번호"} autoComplete="new-password" type="password" name="password" placeholder="비밀번호" />
+        <button type="submit">제출</button>
+      </form>
+    </section>
+  );
 }
 
-export default SignIn
+export default SignIn;
